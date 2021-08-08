01a90e235c56e31f18f509c0b8caa914f8758140
ming <2919413154@qq.com>
2021-08-08 16:04:15 +0800

    44commit

---
+++ src/main/java/com/mylearn/gitTest1.java
@@ -0,0 +1,12 @@
+package com.mylearn;
+
+/**
+ * @author
+ * @create 2021-08-08-14:04
+ */
+public class gitTest1 {
+    public static void main(String[] args) {
+        System.out.println("hello! Git");
+        System.out.println("22222");
+    }
+}
